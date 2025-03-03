0c3e45ecfe25ed2230164c9547374129a51f75c1
doug <dougcr95@gmail.com>
2025-03-02 21:16:58 -0400

    feat: add date utils

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pywmm",
-    version="0.1.0",
+    version="0.2.1",
     description="World Magnetic Model (WMM) calculations",
     author="Douglas Rojas",
     author_email="dougcr95@gmail.com",
